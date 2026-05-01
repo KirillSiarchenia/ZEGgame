79230af875272a87077643d7e63ac23e28a500fd
KirillSiarchenia <kirill.siarchenia@gmail.com>
2026-05-01 22:50:42 +0200

    Changed file structure

--- game/js/maze/mazeObjects.js
+++ game/js/maze/mazeObjects.js
@@ -1,6 +1,6 @@
 const mazeItemsData = {
     "map1": [
         { libId: "maze_coin", x: 17, y: 8 },
-        { libId: "maze_medkit", x: 17, y: 7 }
+        { libId: "maze_medkit", x: 17, y: 7 },
     ],
 };
